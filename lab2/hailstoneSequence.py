5b3deefd4cf0a3ce6b5011e3d5f818f547c7f488
edatan9 <esintaneda@gmail.com>
2026-03-05 13:44:39 +0300

    Implement step counter in hailstone sequence
    Added a step counter to track the number of iterations in the hailstone sequence.

--- lab2/hailstoneSequence.py
+++ lab2/hailstoneSequence.py
@@ -1,7 +1,9 @@
 yourNum = input("Enter an integer number ")
 x = int(yourNum)
+steps=0
 
 while x > 1:
+    steps+=1
     print(str(x), end=" → ")
 
     if x % 2 == 0:
@@ -9,4 +11,6 @@ while x > 1:
     else:
         x = 3 * x + 1
 
+
 print(x)
+print("total steps: ", steps)
